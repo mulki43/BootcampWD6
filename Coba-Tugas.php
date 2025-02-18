0e354e470dcdac22bed0f078b7c6313ebb96039d
mulki43 <79080759+mulki43@users.noreply.github.com>
2025-02-18 13:08:49 +0700

    Add files via upload

---
+++ Coba-Tugas.php
@@ -0,0 +1,9 @@
+<?php
+ $pageTitle = "Arah<br>Creative";
+?>
+<!DOCTYPE html>
+<html>
+ <body>
+ <p style="color: rgb(17, 100, 255); font-size: 100px; font-weight: 1000;line-height: 80%; text-align: right;"><?php echo $pageTitle; ?></h1>
+ </body>
+</html>
---
+++ index.php
@@ -0,0 +1,165 @@
+<?php
+ $pageTitle = "Arah<br>Creative";
+?>
+<!doctype html>
+<html lang="id">
+  <head>
+    <link rel="icon" type="Arah Icon" href="images/Icon.png">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Arah Creative Lab.</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/ArahCreative-Style.css">
+    <script src="js/ArahCreative-script.js"></script>
+  </head>
+  <header>
+    <nav class="navbar navbar-expand-lg nav-transparant fixed-top"> 
+        <div class="container-fluid row">
+            <div class="col-lg-1  logo-filter">
+                <img src="images/logo-1.webp" style="height: 40px; padding-left: 15px;">
+            </div>
+            <div class="col-lg-3 " style="margin-left: -60px; margin-top: 10px;">
+                <h1 class="judul-web">
+                    Arah <br>
+                    Creative <br>
+                    Lab. <br>
+                </h1>
+            </div>
+            <div class="col-lg-4 ">
+                <div class="search-container">
+                    <input type="text" class="search-input" placeholder="Search" style="font-weight: 700;">
+                    <span class="search-icon">🎨</span>
+                </div>
+            </div>
+            <div class="col-lg-4 " style="padding-left: 120px;">
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                      <li class="nav-item">
+                        <a class="nav-link active nav-active" style="color: rgb(17, 100, 255)" aria-current="page" href="index.html">Home</a>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Our Portfolio
+                        </a>
+                        <ul class="dropdown-menu drop-transparant">
+                          <li><a style="font-weight: 400;" class="dropdown-item category-css" href="Not-Avaiable.html">Cinematic Multimedia</a></li>
+                          <li><a style="font-weight: 400;" class="dropdown-item category-css" href="Not-Avaiable.html">Visual Design</a></li>
+                          <li><a style="font-weight: 400;" class="dropdown-item category-css" href="Not-Avaiable.html">Branding</a></li>
+                          <li><a class="dropdown-item category-css" href="All Content.html">All Content</a></li>
+                        </ul>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="Pricing.html">Pricing</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="Contact.html">Contact</a>
+                      </li>
+                    </ul>
+                  </div>
+            </div>
+        </div>
+      </nav>
+  </header>
+  <body>
+    <div class="row container-fluid">
+        <div class="col-lg-6">
+            <img src="images/Arah-Web-D1.webp" style="width: 700px; margin: -260px 0px 0px 150px;">
+            <img src="images/gif3.gif" style=" width: 600px; margin: -45px 0px 0px 20px;" class="">
+        </div>
+    <div class="col-lg-6 " style="margin-top: 130px">
+        <img class="" src="images/gif4.gif" style="width: 200px; margin-left: 440px">
+        <p style="color: rgb(17, 100, 255); font-size: 100px; font-weight: 1000;line-height: 80%; text-align: right;"><?php echo $pageTitle; ?></p>
+    </div>
+    </div>
+    <div class="row" style=" margin-top: 60px; height: 315px; background: linear-gradient(to right ,#043f99, hwb(216 5% 5%));">
+      <div class="col-lg-12 text-center" style="margin: 35px 0px 80px 0px;">
+        <p style="color: rgb(255, 255, 255); font-size: 35px; font-weight: 700;line-height: 80%;" class="text-center">Our Client</p>
+        <img src="images/Client4.webp" style="width: 400px; padding-left: 5px;">
+    </div>
+    </div>
+    <div style="background-image: url(images/Back-2.webp); background-size: cover; background-position: 0px -200px;">
+    <div class="card-group container-fluid empat-tahap-1">
+      <div class="card text-center mb-3">
+        <img src="images/Animasi-1.gif" class="card-img-top" style="width: 150px; margin: auto; display: block;">
+        <div class="card-body">
+          <h5 class="card-title">Meet & Explore</h5>
+          <p class="card-text mx-3 my-3">Kami mendengarkan ide dan kebutuhan Anda untuk menciptakan konsep yang tepat.</p>
+        </div>
+      </div>
+      <div class="card text-center mb-3 " >
+        <img src="images/Animasi-2.gif" class="card-img-top" style="width: 150px; margin: auto; display: block;">
+        <div class="card-body">
+          <h5 class="card-title">Create & Refine</h5>
+          <p class="card-text mx-3 my-3">Dari konsep hingga desain akhir, kami menghidupkan visi Anda dengan kreativitas tanpa batas.</p>
+        </div>
+      </div>
+      <div class="card text-center mb-3 " >
+        <img src="images/Animasi-3.gif" class="card-img-top" style="width: 150px; margin: auto; display: block;">
+        <div class="card-body">
+          <h5 class="card-title">Launch & Elevate</h5>
+          <p class="card-text mx-3 my-3">Waktunya membawa brand Anda ke level berikutnya dengan strategi yang tepat sasaran.</p>
+        </div>
+      </div>
+      <div class="card text-center mb-3 " >
+        <img src="images/Animasi-4.gif" class="card-img-top" style="width: 150px; margin: auto; display: block;">
+        <div class="card-body">
+          <h5 class="card-title">Satisfaction & Trust</h5>
+          <p class="card-text mx-3 my-3">Kami memastikan hasil yang memukau dan pengalaman yang menyenangkan.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row text-center" style="margin-top: -25px;">
+      <div class="col-lg-4"><img class="service-1 service-1-merah" style="margin-right: -300px;" src="images/Services (1).png"></div>
+      <div class="col-lg-4"><img class="service-1 service-1-pink text-center" src="images/Services (2).png"></div>
+      <div class="col-lg-4"><img class="service-1 service-1-hijau" style="margin-left: -300px;" src="images/Services (3).png"></div>
+    </div>
+    <div class="row text-center" style="margin-top: -20px;margin-bottom: 40px;">
+      <div class="col-lg-6"><img style="margin-left: 370px;" class="service-1 service-1-orange" src="images/Services (4).png"></div>
+      <div class="col-lg-6"><img style="margin-left: -370px;" class="service-1 service-1-ungu " src="images/Services (5).png"></div>
+    </div>
+
+    <footer class="container-fluid">
+      <div class="container-fluid isi-footer">
+        <div class="row">
+          <div class="col-lg-4" style="line-height: 30px; margin-right: -250px;">
+            <div>
+              <h5>Halaman</h5>
+            </div>
+            <div>
+              <a style="color: #ffffff; font-size: 15px;" href="index.html">Home</a>
+            </div>
+            <div>
+              <a style="color: #ffffff; font-size: 15px;" href="All Content.html">Our Portfolio</a>
+            </div>
+            <div>
+              <a style="color: #ffffff; font-size: 15px;" href="Pricing.html">Pricing</a>
+            </div>
+            <div>
+              <a style="color: #ffffff; font-size: 15px;" href="Contact.html">Contact</a>
+            </div>
+          </div>
+          <div class="col-lg-4" style="margin-right: 250px;">
+            <div>
+              <h5>Contact Us</h5>
+              <p>Email: arahmail@gmail.com</p>
+              <p>Telp: +62 882-4229-9791</p>
+              <p>Alamat: Ngelarensari A-58. Condongcatur, Depok, Sleman, DI Yogyakarta.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 lokasi-kami">
+            <h5>Lokasi Kami</h5>
+            <iframe 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.2386135579864!2d110.40164577385941!3d-7.764500992254687!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a5918b9911ac5%3A0x931c9d4dc2e16501!2zS29zdCBFeGNsdXNpdmUgc3lhbmFyYSjqp4vqpo_qprrqprTqprHqp4DqpqDqp4Dqpox46qaV6qeA6qat6qa46qax6qa26qau6qaz6qa66qax6qa-6qak6qarKQ!5e0!3m2!1sid!2sid!4v1739702454032!5m2!1sid!2sid" 
+              width="600px" 
+              height="130" 
+              style="border:0;" 
+              allowfullscreen="" 
+              loading="lazy">
+            </iframe>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
